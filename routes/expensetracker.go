bb3ea9553a3f89effcb376f1541594ce94f8a52e
yashdy03 <yashdy03@gmail.com>
2023-12-15 03:33:43 +0530

    crud routes

---
+++ routes/expensetracker.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"strconv"
+
+	"gofr.dev/pkg/gofr"
+)
+
+type Expense struct {
+	ID          int     `json:"id"`
+	Description string  `json:"description"`
+	Amount      float64 `json:"amount"`
+}
+
+func main() {
+	app := gofr.New()
+
+	app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
+
+		return "Hello World!", nil
+	})
+
+	//post a new expense
+	app.POST("/addExpense", func(ctx *gofr.Context) (interface{}, error) {
+		id := ctx.PathParam("id")
+
+		// Read JSON data from the request body
+		var requestPayload struct {
+			Description string  `json:"description"`
+			Amount      float64 `json:"amount"`
+		}
+
+		// Insert data into the 'expenses' table
+		_, err := ctx.DB().ExecContext(ctx, "INSERT INTO expenses (description, amount) VALUES (?, ?)", requestPayload.Description, requestPayload.Amount)
+
+		return id, err
+	})
+
+	//get all expenses
+	app.GET("/allExpenses", func(ctx *gofr.Context) (interface{}, error) {
+		var expenses []Expense
+
+		rows, err := ctx.DB().QueryContext(ctx, "SELECT * FROM expenses")
+		if err != nil {
+			return nil, err
+		}
+		for rows.Next() {
+			var expense Expense
+			if err := rows.Scan(&expense.ID, &expense.Description, &expense.Amount, &expense.Date); err != nil {
+				return nil, err
+			}
+
+			expenses = append(expenses, expense)
+		}
+
+		return expenses, nil
+	})
+
+	app.PUT("/putExpense/{id}", func(ctx *gofr.Context) (interface{}, error) {
+		// Extract the expense ID from the URL parameters
+		expenseID := ctx.PathParam("id")
+
+		// Parse the expenseID to an integer (assuming it's an integer)
+		id, err := strconv.Atoi(expenseID)
+		if err != nil {
+			return nil, err
+		}
+
+		// Fetch the updated expense data from the request body (assuming JSON)
+		var updatedExpense Expense
+
+		// Perform the update in the database
+		_, err = ctx.DB().ExecContext(ctx, "UPDATE expenses SET description=?, amount=? WHERE id=?", updatedExpense.Description, updatedExpense.Amount, id)
+		if err != nil {
+			return nil, err
+		}
+
+		// Return a success message or updated expense data
+		return "Expense updated successfully", nil
+	})
+
+	app.DELETE("/delExpense/{id}", func(ctx *gofr.Context) (interface{}, error) {
+		// Extract expense ID from the URL parameters
+		id := ctx.PathParam("id")
+
+		// Execute the DELETE query
+		_, err := ctx.DB().ExecContext(ctx, "DELETE FROM expenses WHERE id=?", id)
+		if err != nil {
+			return nil, err
+		}
+
+		return "Expense deleted successfully", nil
+	})
+
+	app.Start()
+}
